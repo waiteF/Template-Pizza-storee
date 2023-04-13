411b9eebf7db6c049b152527259ed0ee17a61387
waiteF <91004866+waiteF@users.noreply.github.com>
2023-04-13 08:36:55 +0300

    Add files via upload

---
+++ main/main.js
@@ -0,0 +1,8 @@
+$(".sort-by").on("click",function(){
+    $(".sort").toggleClass("active")
+    $(".sort-by i").toggleClass("rotation")
+})
+$(".type").on("click",function(){
+    $(".menu").toggleClass("active-menu")
+    $(".type i").toggleClass("rotation")
+})
